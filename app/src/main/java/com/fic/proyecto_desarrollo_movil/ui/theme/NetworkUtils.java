d1bd98c78a5f54e137da466a741444a2ef9d9046
LuisAceves04 <luissk392@gmail.com>
2025-12-17 19:21:28 -0700

    lol

--- app/src/main/java/com/fic/proyecto_desarrollo_movil/ui/theme/NetworkUtils.java
+++ app/src/main/java/com/fic/proyecto_desarrollo_movil/ui/theme/NetworkUtils.java
@@ -10,7 +10,7 @@ import java.net.URL;
 
 public class NetworkUtils {
 
-    private static final String BASE_URL = "http://192.168.100.27/parkmanager/api/";
+    private static final String BASE_URL = "http://10.0.2.2/parkmanager/api/";
     private static final String TAG = "NetworkUtils";
 
     public static String sendPostRequest(String endpoint, String jsonInput) {
